179decc0feb2403daf09d555caa2fb0fea7132a1
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-03-14 15:28:40 +0100

    Song Entity

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SongRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class SongController extends AbstractController
 {
     #[Route('/song', name: 'song.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, SongRepository $repository): Response
     {
+        $songs = $repository->findAll();
+        dd($songs);
+        return new Response('Songs');
     }
 
     #[Route('/song/{slug}-{id}', name: 'song.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
