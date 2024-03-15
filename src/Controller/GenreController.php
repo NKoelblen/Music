4c40e5efe2968816fa55419e866e28d9d541c27b
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-03-15 13:09:54 +0100

    Genres
    Controller, Entity, Templates & CRUD

---
+++ src/Controller/GenreController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GenreRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+#[Route('/genre', name: 'genre.')]
+class GenreController extends AbstractController
+{
+    #[Route(name: 'index')]
+    public function index(Request $request, GenreRepository $repository): Response
+    {
+        $genres = $repository->findAll();
+        return $this->render(
+            'genre/index.html.twig',
+            [
+                'genres' => $genres
+            ]
+        );
+    }
+
+    #[Route('/{slug}-{id}', name: 'show', requirements: ['id' => Requirement::POSITIVE_INT, 'slug' => Requirement::ASCII_SLUG])]
+    public function show(Request $request, string $slug, int $id, GenreRepository $repository): Response
+    {
+        $genre = $repository->find($id);
+        if ($genre->getSlug() !== $slug) {
+            return $this->redirectToRoute('genre.show', ['slug' => $genre->getSlug(), 'id' => $genre->getId()]);
+        }
+        return $this->render(
+            'genre/show.html.twig',
+            [
+                'genre' => $genre,
+            ]
+        );
+    }
+}
--- src/Form/GenreType.php
+++ src/Form/GenreType.php
@@ -2,18 +2,17 @@
 
 namespace App\Form;
 
-use App\Entity\Song;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
-class SongType extends AbstractType
+class GenreType extends AbstractType
 {
     public function __construct(private FormListenerFactory $formListenerFactory)
     {
@@ -24,17 +23,26 @@ class SongType extends AbstractType
         $builder
             ->add('title')
             ->add('slug', TextType::class, ['required' => false])
-            ->add('link')
             ->add('save', SubmitType::class)
             ->addEventListener(FormEvents::PRE_SUBMIT, $this->formListenerFactory->autoslug('title'))
-            ->addEventListener(FormEvents::POST_SUBMIT, $this->formListenerFactory->autoCreatedAt())
         ;
     }
+
+    public function autoslug(PreSubmitEvent $event): void
+    {
+        $data = $event->getData();
+        if (empty ($date['slug'])):
+            $slugger = new AsciiSlugger();
+            $data['slug'] = strtolower($slugger->slug($data['title']));
+            $event->setData($data);
+        endif;
+    }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Song::class,
+            'data_class' => Genre::class,
         ]);
     }
 }
